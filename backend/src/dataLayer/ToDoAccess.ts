e689ecdff1424732b1684c102bb27e27d18bb425
GitGud31 <mhamedlehbab@gmail.com>
2021-01-08 16:29:38 +0100

    moved components to DataLayer, not tested on deployment yet

--- backend/src/dataLayer/ToDoAccess.ts
+++ backend/src/dataLayer/ToDoAccess.ts
@@ -1,17 +1,31 @@
 import * as AWS from "aws-sdk";
-import {DocumentClient} from "aws-sdk/clients/dynamodb";
-import {Types} from 'aws-sdk/clients/s3';
-import {TodoItem} from "../models/TodoItem";
-import {TodoUpdate} from "../models/TodoUpdate";
+import * as AWSXray from "aws-xray-sdk";
+
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+import { Types } from 'aws-sdk/clients/s3';
+import { TodoItem } from "../models/TodoItem";
+import { TodoUpdate } from "../models/TodoUpdate";
+
+
+const XAWS = AWSXray.captureAWS(AWS);
 
 export class ToDoAccess {
+    static s3BucketName: any = process.env.S3_BUCKET_NAME;
+    static todosTable = process.env.TODOS_TABLE;
+    static attachmentUrl = `https://${ToDoAccess.s3BucketName}.s3.amazonaws.com/`
+
     constructor(
-        private readonly docClient: DocumentClient = new AWS.DynamoDB.DocumentClient(),
-        private readonly s3Client: Types = new AWS.S3({signatureVersion: 'v4'}),
+
+        //CHANGED AS REQUIRED.
+        private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
+
+        private readonly s3Client: Types = new AWS.S3({ signatureVersion: 'v4' }),
         private readonly todoTable = process.env.TODOS_TABLE,
         private readonly s3BucketName = process.env.S3_BUCKET_NAME) {
     }
 
+
+    //
     async getAllToDo(userId: string): Promise<TodoItem[]> {
         console.log("Getting all todo");
 
@@ -33,6 +47,7 @@ export class ToDoAccess {
         return items as TodoItem[]
     }
 
+    //
     async createToDo(todoItem: TodoItem): Promise<TodoItem> {
         console.log("Creating new todo");
         const params = {
@@ -46,6 +61,7 @@ export class ToDoAccess {
         return todoItem as TodoItem;
     }
 
+    //
     async updateToDo(todoUpdate: TodoUpdate, todoId: string, userId: string): Promise<TodoUpdate> {
         console.log("Updating todo");
 
@@ -68,17 +84,18 @@ export class ToDoAccess {
                     ":c": true,
                 },
                 ReturnValues: "ALL_NEW"
-            },function(err,data){
+            }, function (err, data) {
                 if (err) console.log(err);
                 else console.log(data);
             }
         ).promise();
-        todoUpdate['name']="zaki";
-        const attributes = result.Attributes;   
+        todoUpdate['name'] = "zaki";
+        const attributes = result.Attributes;
 
         return attributes as TodoUpdate;
     }
 
+    //
     async deleteToDo(todoId: string, userId: string): Promise<string> {
         console.log("Deleting todo");
 
@@ -96,6 +113,7 @@ export class ToDoAccess {
         return "" as string;
     }
 
+    //
     async generateUploadUrl(todoId: string) {
         console.log("Generating URL");
 
@@ -111,9 +129,27 @@ export class ToDoAccess {
             attachmentUrl
         }
     }
-    async updateTodoItem(updatedTodoItem) {
+
+    //
+    async updateTodoItem(userId, todoId, attachmentUrl) {
+
+        const updatedTodoItem = {
+
+            ///todoTable is now a component of dataLayer
+            TableName: ToDoAccess.todosTable,
+            Key: {
+                "userId": userId,
+                "todoId": todoId
+            },
+            UpdateExpression: 'set attachmentUrl=:a',
+            ExpressionAttributeValues: {
+                ':a': attachmentUrl
+            },
+            ReturnValues: 'UPDATED_NEW'
+        }
+        
+        console.log('updating TodoItem', updatedTodoItem)
         return this.docClient.update(updatedTodoItem).promise()
-      }
-    
-    
+    }
+
 }
